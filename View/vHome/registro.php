80d80629623338aa16850956c69785a2781f19b6
Fernanda Fajardo <nanda199784@gmail.com>
2026-01-31 18:52:44 -0600

    Creación de la pantalla registro

---
+++ View/vHome/registro.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SGH</title>
+
+    <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../assets/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="../assets/css/style.css">
+   
+    <link rel="shortcut icon" href="../assets/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+          <div class="row flex-grow">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-light text-left p-5">
+                <div class="brand-logo">
+                  <img src="../assets/images/Logo.png">
+                </div>
+                <h4>¿Nuevo aquí?</h4>
+                <h6 class="font-weight-light">Registrarse es facíl. Solo te toma un par de pasos.</h6>
+                <form class="pt-3">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Usuario">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Correo">
+                  </div>
+                  <div class="form-group">
+                    <select class="form-select form-select-lg" id="exampleFormControlSelect2">
+                      <option>País</option>
+                      <option>Estados Unidos</option>
+                      <option>Reino Unido</option>
+                      <option>Costa Rica</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Contraseña">
+                  </div>
+                  <div class="mb-4">
+                    <div class="form-check">
+                      <label class="form-check-label text-muted">
+                        <input type="checkbox" class="form-check-input"> Estoy de acuerdo con todos los Terminos y Condiciones </label>
+                    </div>
+                  </div>
+                  <div class="mt-3 d-grid gap-2">
+                    <a class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">REGISTRARSE</a>
+                  </div>
+                  <div class="text-center mt-4 font-weight-light"> ¿Ya tienes una cuenta? <a href="inicio_sesion.php" class="text-primary">Iniciar Sesión</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/misc.js"></script>
+    <script src="../assets/js/settings.js"></script>
+    <script src="../assets/js/todolist.js"></script>
+    <script src="../assets/js/jquery.cookie.js"></script>
+  </body>
+</html>
