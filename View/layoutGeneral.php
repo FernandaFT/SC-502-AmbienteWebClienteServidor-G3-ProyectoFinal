77f4a3291e0c82624fe983b3d800f57f2eaa5cd9
ArroyoFD <168361850+ArroyoFD@users.noreply.github.com>
2026-04-16 15:17:55 -0600

    Corrección conflicto en notificaciones

--- View/layoutGeneral.php
+++ View/layoutGeneral.php
@@ -44,7 +44,7 @@ function JSGeneral()
     <script src="../assets/funciones/horas.js"></script>
     <script src="../assets/funciones/vacaciones.js"></script>
     <script src="../assets/funciones/permisos.js"></script>
-    <script src="../assets/funciones/notificaciones.js"></script>';
+    <script src="../assets/funciones/notificaciones.js"></script>
     <script src="../assets/funciones/reporte.js"></script>';
 }
 
